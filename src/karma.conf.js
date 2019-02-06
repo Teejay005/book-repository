d3a332508574ce738fa6b3e9f8025bb71356160a
MacBook Pro <mac@macbooks-mbp.home>
2019-02-06 22:55:54 +1000

    add modules for running test in headless browser

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -9,6 +9,7 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
+      require('karma-phantomjs-launcher'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
@@ -26,6 +27,6 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: true
   });
 };
